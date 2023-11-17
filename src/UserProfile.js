77f3e07e2a4d3c2c4d90be97bf7575a4d95b53e8
Arijeet Nayak <arrild1@gmail.com>
2023-11-17 19:50:32 +0530

    Interaction buttons

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './UserProfile.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faThumbsUp, faEye, faHeart } from '@fortawesome/free-solid-svg-icons';
 
-function UserProfile() {
+const UserProfile = () => {
+  // Function to handle button clicks
+  const handleButtonClick = (buttonType) => {
+    // Handle button click based on the buttonType
+    console.log(`Button clicked: ${buttonType}`);
+  };
+
   return (
     <div className="UserProfile">
+      {/* Navigation bar */}
       <nav className="navbar">
         <div className="logo">
           <img src="https://img.freepik.com/free-vector/colorful-bird-illustration-gradient_343694-1741.jpg?size=626&ext=jpg&ga=GA1.1.1196953875.1699991651&semt=sph" alt="Logo" />
@@ -12,10 +21,11 @@ function UserProfile() {
         </div>
         <button className="nav-button"><strong>Courses</strong></button>
       </nav>
+
+      {/* User profile section */}
       <div className="background">
-      <div className="profile-picture"></div>
+        <div className="profile-picture"></div>
         <div className="profile">
-          
           <div className="profile-info">
             <span className="name">Name</span>
             <div className="followers-followings">
@@ -30,20 +40,39 @@ function UserProfile() {
             </div>
             <span className="bio">Bio line 1</span>
             <span className="bio">Bio line 2</span>
+
+            {/* Interaction buttons */}
             <div className="interaction-buttons">
-              <button className="star-button">Star</button>
-              <button className="thumbs-up-button">Thumbs Up</button>
-              <button className="eye-button">Eye</button>
-              <button className="heart-button">Heart</button>
+            <button className="star-button button" onClick={() => handleButtonClick('star')}>
+          <FontAwesomeIcon icon={faStar} className="icon" />
+          <span className="button-number">1</span>
+        </button>
+        <button className="thumbs-up-button button" onClick={() => handleButtonClick('thumbsUp')}>
+          <FontAwesomeIcon icon={faThumbsUp} className="icon" />
+          <span className="button-number">2</span>
+        </button>
+        <button className="eye-button button" onClick={() => handleButtonClick('eye')}>
+          <FontAwesomeIcon icon={faEye} className="icon" />
+          <span className="button-number">3</span>
+        </button>
+        <button className="heart-button button" onClick={() => handleButtonClick('heart')}>
+          <FontAwesomeIcon icon={faHeart} className="icon" />
+          <span className="button-number">4</span>
+        </button>
             </div>
           </div>
         </div>
       </div>
+
+      {/* Posts section */}
       <div className="posts-section">
         <span className="post-count">Total Posts: 10</span>
         <div className="post">
           <span className="topic">Post Topic</span>
-          <span className="content">Post Content</span>
+          <span className="content">
+            Post Content with some details. Add more text here.
+            <FontAwesomeIcon icon={faThumbsUp} className="thumbs-up-icon" />
+          </span>
           <div className="author-info">
             <span className="author-name">Author Name</span>
             <div className="post-details">
@@ -56,6 +85,6 @@ function UserProfile() {
       </div>
     </div>
   );
-}
+};
 
 export default UserProfile;
